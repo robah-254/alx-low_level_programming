3e7ce2e24725d8f426ca5acc4ef364a970dafe32
robah-254 <njugunarobert1999@gmail.com>
2023-03-16 09:45:40 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+char a;
+int b;
+long int c;
+long long int d;
+float f;
+prinf("size of a char: %lu byte(s)\n", (unsigned long)size of (a));
+prinf("size of an int: %lu byte(s)\n", (unsigned long)size of (b));
+prinf("size of a long int: %lu byte(s)\n", (unsigned long)size of (c));
+prinf("size of a long long int: %lu byte(s)\n", (unsigned long)size of (d));
+prinf("size of a float: %lu byte(s)\n", (unsigned long)size of (f));
+return (0);
+}
