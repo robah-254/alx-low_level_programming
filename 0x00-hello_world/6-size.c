26f114e5e99fb0cd0bc6491b26d8e7ca23c92fc7
robah-254 <njugunarobert1999@gmail.com>
2023-03-16 09:53:21 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,15 +6,15 @@
  */
 int main(void)
 {
-char a;
-int b;
-long int c;
-long long int d;
-float f;
-prinf("size of a char: %lu byte(s)\n", (unsigned long)size of (a));
-prinf("size of an int: %lu byte(s)\n", (unsigned long)size of (b));
-prinf("size of a long int: %lu byte(s)\n", (unsigned long)size of (c));
-prinf("size of a long long int: %lu byte(s)\n", (unsigned long)size of (d));
-prinf("size of a float: %lu byte(s)\n", (unsigned long)size of (f));
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+prinf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+prinf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
+prinf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+prinf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+prinf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
