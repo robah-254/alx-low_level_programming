c41a8f7be33c63cf6a24e2433791af5b0433641d
robah-254 <njugunarobert1999@gmail.com>
2023-03-17 12:05:32 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,19 +1,24 @@
 #include<stdio.h>
 /**
- * main - Entry point
+ *main - Entry point
  *
  * Return: always 0
  */
 int main(void)
 {
 	int n = 97;
+	int m = 65;
 
 	while (n <= 122)
 	{
 		putchar(n);
 		n++;
 	}
-	putchar("\n");
+	while (m <= 90)
+	{
+		putchar(m);
+		m++;
+	}
+	putchar('\n');
 	return (0);
 }
-
