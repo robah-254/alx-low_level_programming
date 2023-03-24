266077eed7b767efbb7ebd0678f9207cda48b1cf
robah-254 <njugunarobert1999@gmail.com>
2023-03-23 17:30:02 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -8,11 +8,11 @@
 
 void print_numbers(void)
 {
-	int i;
+	char c;
 
-	for (i = 0; i < 10; i++)
+	for (c = '0'; c <= '9'; c++)
 	{
-		_putchar(i);
+		_putchar(c);
 	}
-	putchar('\n');
+	_putchar('\n');
 }
