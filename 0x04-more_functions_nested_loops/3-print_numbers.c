9b0e641bfa08dde49219836480b75a03cfc21ac0
robah-254 <njugunarobert1999@gmail.com>
2023-03-23 17:25:11 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - prints numbers 0 to 9
+ *
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i);
+	}
+	putchar('\n');
+}
