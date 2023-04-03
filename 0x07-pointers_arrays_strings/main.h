da99f92bb722a3f44ab71e4de39754a8344af698
robah-254 <njugunarobert1999@gmail.com>
2023-04-03 01:21:23 -0700

    main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,8 +2,9 @@
 #define "MAIN_H"
 
 #include<stdio.h>
+#include<unistd.h>
 
-int _putchar();
+int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
